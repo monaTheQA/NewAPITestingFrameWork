1254e598a9492eb25251d9830068b640ce2dc5bb
mona sinha <mona.olabz@gmail.com>
2020-04-26 16:20:58 +0530

    this is the second commit

--- src/test/java/APIFrameWorkTestCases/CreateCustomerTest.java
+++ src/test/java/APIFrameWorkTestCases/CreateCustomerTest.java
@@ -12,11 +12,15 @@ public class CreateCustomerTest {
 		
 		Response response = given().auth().basic("sk_test_4eC39HqLyjWDarjtT1zdp7dc", "").formParam("email", "apiTest@test.com").formParam("name", "mona")
 		.post("https://api.stripe.com/v1/customers");
-		
+		//Add new comment
 		response.prettyPrint();
 		String customerId = response.jsonPath().get("id");
+		String customerName = response.jsonPath().get("name");
 		System.out.println(response.statusCode());
 		System.out.println("New Customer Id is : "+ customerId);
+		System.out.println("New Customer Name is : "+ customerName);
+
+		
 		
 
 	}
